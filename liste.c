752f370a30cdee7df27ead1820b060b3ce0e9333
VedsonDenisJoseph <vedson-denis.joseph.1@ens.etsmtl.ca>
2024-11-11 20:34:32 -0500

    finished liste.c

--- liste.c
+++ liste.c
@@ -3,6 +3,7 @@
 //
 #include "liste.h"
 #include <assert.h>
+#include <limits.h>
 #include <stdbool.h>
 #include <stdio.h>
 
@@ -16,6 +17,7 @@
 //  ----------------------------------------
 
 void liste_init(Liste liste, int limite) {
+<<<<<<< Updated upstream
 	mdwqmdpo
 }
 
@@ -29,9 +31,27 @@ int liste_get_limite(const Liste liste) {
 
 void liste_lire_noeud(const Liste liste, int indice, Noeud dest) {
 	// A completer
+=======
+	liste[LISTE_INDICE_LIGNE_META][LISTE_INDICE_NB_ELEMENTS] = 0;
+	liste[LISTE_INDICE_LIGNE_META][LISTE_INDICE_LIMITE] = limite;
 }
 
+int liste_get_nb_elements(const Liste liste) {
+	return liste[LISTE_INDICE_LIGNE_META][LISTE_INDICE_NB_ELEMENTS];
+}
+
+int liste_get_limite(const Liste liste) {
+	return liste[LISTE_INDICE_LIGNE_META][LISTE_INDICE_LIMITE];
+}
+
+void liste_lire_noeud(const Liste liste, int indice, Noeud dest) {
+	noeud_copier(dest, liste[indice]);
+>>>>>>> Stashed changes
+}
+
+
 void liste_ecrire_noeud(Liste liste, int indice, const Noeud src) {
+<<<<<<< Updated upstream
 	// A completer
 }
 
@@ -45,20 +65,68 @@ bool liste_est_pleine(const Liste liste) {
 
 void liste_afficher(const Liste liste) {
 	// A completer
+=======
+	noeud_copier(liste[indice], src);
+}
+
+bool liste_est_vide(const Liste liste) {
+	bool liste_vide = false;
+
+	if (liste_get_nb_elements(liste) == 0)
+		liste_vide = true;
+
+	return liste_vide;
+}
+
+bool liste_est_pleine(const Liste liste) {
+	bool liste_pleine = false;
+
+	if (liste_get_nb_elements(liste) != 0)
+		liste_pleine = true;
+
+	return liste_pleine;
+}
+
+void liste_afficher(const Liste liste) {
+	int limite = liste_get_nb_elements(liste);
+
+	for (int ligne = 1; ligne <= limite; ligne++)
+		noeud_afficher(liste[ligne]);
+>>>>>>> Stashed changes
 }
 
 int liste_chercher_noeud(const Liste liste, const Noeud noeud) {
-	// A completer
+	int indice_noeud = INDICE_NON_TROUVE;
+	int limite = liste_get_limite(liste);
+
+	for (int indice = 1; indice <= limite; indice++)
+		if (noeud_sont_egaux(noeud, liste[indice]))
+			indice_noeud = indice;
+
+	return indice_noeud;
 }
 
 int liste_chercher_noeud_min_distance(const Liste liste) {
-	// A completer
+	int indice_du_plus_petit_eval = INDICE_NON_TROUVE;
+	int min_eval = INT_MAX;
+
+	int limite = liste_get_limite(liste);
+	for (int indice = 1; indice <= limite; indice++)
+		if (min_eval > noeud_get_evaluation(liste[indice])) {
+			min_eval = noeud_get_evaluation(liste[indice]);
+			indice_du_plus_petit_eval = indice;
+		}
+
+	return indice_du_plus_petit_eval;
 }
 
 int liste_ajouter_noeud(Liste liste, const Noeud noeud) {
-	// A completer
+	liste[LISTE_INDICE_LIGNE_META][LISTE_INDICE_NB_ELEMENTS]++;
+	int indice_element_du_noeud_a_ajouter = liste_get_nb_elements(liste);
+
+	return indice_element_du_noeud_a_ajouter;
 }
 
 void liste_supprimer_noeud(Liste liste, int indice) {
-	// A completer
+	liste[LISTE_INDICE_LIGNE_META][LISTE_INDICE_NB_ELEMENTS]--;
 }
